b22d35c262aaa641595c8997631fad7479935f4a
lol50002 <artem130500@mail.ru>
2016-05-21 21:36:45 +0300

    dfg
    dg

---
+++ Hanoy/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.disk1 = new System.Windows.Forms.PictureBox();
+            this.disk2 = new System.Windows.Forms.PictureBox();
+            this.disk3 = new System.Windows.Forms.PictureBox();
+            this.disk4 = new System.Windows.Forms.PictureBox();
+            this.knopkapls = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.disk1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.disk2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.disk3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.disk4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.pictureBox1.Location = new System.Drawing.Point(59, 481);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(281, 33);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.pictureBox2.Location = new System.Drawing.Point(768, 481);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(281, 33);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.pictureBox3.Location = new System.Drawing.Point(402, 481);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(281, 33);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.pictureBox4.Location = new System.Drawing.Point(177, 244);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(27, 231);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.disk1_MouseDown);
+            this.pictureBox4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.disk1_MouseMove);
+            this.pictureBox4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.disk1_MouseUp);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.pictureBox5.Location = new System.Drawing.Point(520, 244);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(27, 231);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.pictureBox6.Location = new System.Drawing.Point(902, 244);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(27, 231);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // disk1
+            // 
+            this.disk1.BackColor = System.Drawing.Color.Black;
+            this.disk1.Location = new System.Drawing.Point(106, 446);
+            this.disk1.Name = "disk1";
+            this.disk1.Size = new System.Drawing.Size(167, 29);
+            this.disk1.TabIndex = 6;
+            this.disk1.TabStop = false;
+            this.disk1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.disk1_MouseDown);
+            this.disk1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.disk1_MouseMove);
+            this.disk1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.disk1_MouseUp);
+            // 
+            // disk2
+            // 
+            this.disk2.BackColor = System.Drawing.Color.Black;
+            this.disk2.Location = new System.Drawing.Point(131, 411);
+            this.disk2.Name = "disk2";
+            this.disk2.Size = new System.Drawing.Size(123, 29);
+            this.disk2.TabIndex = 9;
+            this.disk2.TabStop = false;
+            // 
+            // disk3
+            // 
+            this.disk3.BackColor = System.Drawing.Color.Black;
+            this.disk3.Location = new System.Drawing.Point(143, 376);
+            this.disk3.Name = "disk3";
+            this.disk3.Size = new System.Drawing.Size(95, 29);
+            this.disk3.TabIndex = 10;
+            this.disk3.TabStop = false;
+            // 
+            // disk4
+            // 
+            this.disk4.BackColor = System.Drawing.Color.Black;
+            this.disk4.Location = new System.Drawing.Point(162, 341);
+            this.disk4.Name = "disk4";
+            this.disk4.Size = new System.Drawing.Size(58, 29);
+            this.disk4.TabIndex = 11;
+            this.disk4.TabStop = false;
+            // 
+            // knopkapls
+            // 
+            this.knopkapls.Location = new System.Drawing.Point(364, 44);
+            this.knopkapls.Name = "knopkapls";
+            this.knopkapls.Size = new System.Drawing.Size(250, 39);
+            this.knopkapls.TabIndex = 12;
+            this.knopkapls.Text = "Авто!";
+            this.knopkapls.UseVisualStyleBackColor = true;
+            this.knopkapls.Click += new System.EventHandler(this.knopkapls_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 591);
+            this.Controls.Add(this.knopkapls);
+            this.Controls.Add(this.disk4);
+            this.Controls.Add(this.disk3);
+            this.Controls.Add(this.disk2);
+            this.Controls.Add(this.disk1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.disk1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.disk2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.disk3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.disk4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox disk1;
+        private System.Windows.Forms.PictureBox disk2;
+        private System.Windows.Forms.PictureBox disk3;
+        private System.Windows.Forms.PictureBox disk4;
+        private System.Windows.Forms.Button knopkapls;
+    }
+}
+
---
+++ Hanoy/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        bool stats = false;
+        bool pop = false;
+
+        private void disk1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (stats == true)
+                disk1.Location = new Point(Cursor.Position.X- this.Location.X, Cursor.Position.Y- this.Location.Y);  
+            if ((peresechenierectangle(disk1,pictureBox5)&&!(peresechenierectangle(disk2,pictureBox5)&&peresechenierectangle(disk3,pictureBox5)&&peresechenierectangle(disk4,pictureBox5))))
+            {
+                disk1.Location = new Point(450, 450);
+            }
+
+        }
+
+        private void disk1_MouseDown(object sender, MouseEventArgs e)
+        {
+            stats = true;
+        }
+
+        private void disk1_MouseUp(object sender, MouseEventArgs e)
+        {
+            stats = false;
+        }
+
+
+        private bool peresechenierectangle(PictureBox a, PictureBox b)
+        {
+            Rectangle yota = a.Bounds;
+            Rectangle yota2 = b.Bounds;
+            if (yota.IntersectsWith(yota2))
+                pop = true;
+            return pop;
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        } 
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {     
+
+
+
+
+
+
+
+
+
+        }
+
+        private void knopkapls_Click(object sender, EventArgs e)
+        {
+            disk4.Location = new Point(890, 341);
+            disk3.Location = new Point(880, 376);
+            disk2.Location = new Point(870, 411);
+            disk1.Location = new Point(850, 446);
+            MessageBox.Show("Ты сдался. Но не переживай. Все получится .Вот схема для постановки 4 дисков, точнее вот сайт https://ru.wikipedia.org/wiki/%D0%A5%D0%B0%D0%BD%D0%BE%D0%B9%D1%81%D0%BA%D0%B0%D1%8F_%D0%B1%D0%B0%D1%88%D0%BD%D1%8F зайди и посмотри!");
+        }
+
+
+    }
+}
